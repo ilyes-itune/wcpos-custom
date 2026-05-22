4cecb7a58def01f1b590eb9f9efdda89ff87caf1
ilyes-itune <ilyes.itune@gmail.com>
2026-05-22 19:47:19 +0200

    Update window.ts

--- src/main/window.ts
+++ src/main/window.ts
@@ -16,7 +16,7 @@ if (isDevelopment) {
 
 let mainWindow: BrowserWindow | null;
 
-const APP_VERSION  = 'WCPOS Custom 5.2';
+const APP_VERSION  = 'WCPOS Custom 5.3';
 const WP_SITE_URL  = 'https://usmm-tir.fr';
 const WP_REST_BASE = 'https://usmm-tir.fr/wp-json/wcpos-custom/v1';
 
