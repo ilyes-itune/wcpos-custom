1bb2cca5ca67db278cb5868f7ec5c845ce3ec0fa
ilyes-itune <ilyes.itune@gmail.com>
2026-06-22 23:21:41 +0200

    Update window.ts Release 1

--- src/main/window.ts
+++ src/main/window.ts
@@ -16,7 +16,7 @@ if (isDevelopment) {
 
 let mainWindow: BrowserWindow | null;
 
-const APP_VERSION  = 'POSTir 5.4.0';
+const APP_VERSION  = 'POSTir 5.4';
 const WP_SITE_URL  = 'https://usmm-tir.fr';
 const WP_REST_BASE = 'https://usmm-tir.fr/wp-json/wcpos-custom/v1';
 
